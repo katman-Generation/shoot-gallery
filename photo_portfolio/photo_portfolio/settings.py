6d7fc5daf917e03817f8aee51d4a5d74d7edad9c
Tatenda  Katema <primewebwizards@gmail.com>
2024-12-11 10:14:32 +0200

    Add Procfile for Heroku deployment

--- photo_portfolio/photo_portfolio/settings.py
+++ photo_portfolio/photo_portfolio/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR, 'staticfiles']
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
