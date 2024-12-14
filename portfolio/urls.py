1b733cad71d4d3743897b3c44799b3644c285dd1
Tatenda  Katema <primewebwizards@gmail.com>
2024-12-14 03:01:38 +0200

    adding the category page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import portfolio_view
+from .views import portfolio_view, category_photos
 
 urlpatterns = [
     path('', portfolio_view, name='portfolio'),
+    path('category/<int:category_id>/', category_photos, name='category_photos'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Photo, Category
 
 def portfolio_view(request):
@@ -6,3 +6,13 @@ def portfolio_view(request):
     photos = Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
     return render(request, 'portfolio/portfolio.html', context)
+
+
+def category_photos(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    photos = Photo.objects.filter(category=category)
+    context = {
+        'category': category,
+        'photos': photos,
+    }
+    return render(request, 'portfolio/category_photos.html', context)
